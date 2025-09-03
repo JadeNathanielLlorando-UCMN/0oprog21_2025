00ea4c747df6bb8e4dd51861d4566fea83ec41d0
JadeNathanielLlorando-UCMN <nathanielllorando03@gmail.com>
2025-09-03 10:07:56 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,25 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        String[] songLines = {
+            "Nakapili na ba ka",
+            "kung kinsa sa among duha",
+            "ang pilion mong makauban",
+            "ipailailas ginikanan",
+            "Gihatag ko na ang tanan",
+            "ug ihatag ko pa ang tanan, apan",
+            "Kung siya man ang mapilian",
+            "wa koy mahimu ana pero",
+            "sakit lang jud kaayu",
+            "kung siya man ang maswertehan",
+            "mupalayu nako ninyu",
+            "ug ihilak nalang sa tago",
+            "pero ug iya kang byaan",
+            "ug iyang binoangan, pag mahitabo",
+            "wala na siyay balikan"
+        };
+
+        for (int i = 0; i < songLines.length; i++) {
+            System.out.println(songLines[i]);
+        }
+    }
+}
